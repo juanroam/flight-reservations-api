d4e7dcd28eb129179d043de30a5be3c2a3e70f8f
juanroam <juanc.rodrigueza@hotmail.com>
2025-04-03 13:00:06 -0500

    Adjust error messages APIError

--- src/main/java/com/juanroam/reservations/enums/APIError.java
+++ src/main/java/com/juanroam/reservations/enums/APIError.java
@@ -4,19 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+/**
+ * This documents the different errors when invoking the API.
+ */
 @Getter
 @AllArgsConstructor
 public enum APIError {
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST,
-            "The are attributes with wrong values"),
+            "There are attributes with wrong values"),
     BAD_FORMAT(HttpStatus.BAD_REQUEST,
-            "The message not have a correct form"),
+            "The message is not formatted correctly"),
     RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND,
             "Reservation not found"),
     RESERVATION_WITH_SAME_ID(HttpStatus.BAD_REQUEST,
-            "There is a reservation with the same id");
+            "There is a reservation with the same ID");
 
+    /**
+     * HTTP Status code.
+     */
     private final HttpStatus httpStatus;
 
+    /**
+     * Text describing the error that occurred.
+     */
     private final String message;
 }
