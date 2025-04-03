71bbf8eb50606850616a01b018c4d9f9cb63dfe7
juanroam <juanc.rodrigueza@hotmail.com>
2025-04-02 22:51:40 -0500

    Base gradle project

---
+++ src/main/java/com/juanroam/flight_reservations_api/FlightReservationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.juanroam.flight_reservations_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReservationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanroam/flight_reservations_api/FlightReservationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanroam.flight_reservations_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
