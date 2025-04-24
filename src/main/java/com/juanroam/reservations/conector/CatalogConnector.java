9803d71863daf9d58267394cb9eb9d0654a30c9b
juanroam <juanc.rodrigueza@hotmail.com>
2025-04-23 20:47:00 -0500

    Logs implementing Log4j

--- src/main/java/com/juanroam/reservations/conector/CatalogConnector.java
+++ src/main/java/com/juanroam/reservations/conector/CatalogConnector.java
@@ -10,7 +10,8 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import io.netty.resolver.DefaultAddressResolverGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -23,6 +24,8 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class CatalogConnector {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CatalogConnector.class);
 
     private final String HOST = "api-catalog";
 
@@ -37,7 +40,7 @@ public class CatalogConnector {
 
     @CircuitBreaker(name = "api-catalog", fallbackMethod = "fallbackGetCity")
     public CityDTO getCity(String code) {
-        System.out.println("Calling to api-catalog");
+        LOGGER.info("calling to api-catalog");
         HostConfiguration hostConfiguration = configuration.getHosts().get(HOST);
         EndpointConfiguration endpointConfiguration = hostConfiguration.getEndpoints().get(ENDPOINT);
 
@@ -72,7 +75,7 @@ public class CatalogConnector {
      * @return
      */
     private CityDTO fallbackGetCity(String code, CallNotPermittedException cnpe) {
-        System.out.println("calling fallbackGetCity-1");
+        LOGGER.debug("calling to fallbackGetCity-1");
 
         return new CityDTO();
     }
@@ -84,7 +87,7 @@ public class CatalogConnector {
      * @return
      */
     private CityDTO fallbackGetCity(String code, Exception exc) {
-        System.out.println("calling fallbackGetCity-2");
+        LOGGER.debug("calling to fallbackGetCity-2");
 
         return new CityDTO();
     }
--- src/main/java/com/juanroam/reservations/controller/ReservationController.java
+++ src/main/java/com/juanroam/reservations/controller/ReservationController.java
@@ -9,6 +9,8 @@ import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ import java.util.List;
 @RequestMapping("/reservation")
 public class ReservationController implements ReservationResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReservationController.class);
+
     private final ReservationService service;
 
     @Autowired
@@ -31,12 +35,14 @@ public class ReservationController implements ReservationResource {
 
     @GetMapping
     public ResponseEntity<List<ReservationDTO>> getReservations() {
+        LOGGER.info("Obtain all the reservations");
         List<ReservationDTO> response = service.getReservations();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ReservationDTO> getReservationById(@Min(1) @PathVariable Long id) {
+        LOGGER.info("Obtain information from a reservation with {}", id);
         ReservationDTO response = service.getReservationById(id);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -44,23 +50,26 @@ public class ReservationController implements ReservationResource {
     @PostMapping
     @RateLimiter(name = "post-reservation", fallbackMethod = "fallbackPost")
     public ResponseEntity<ReservationDTO> save(@Valid @RequestBody ReservationDTO reservation) {
+        LOGGER.info("Saving new reservation");
         ReservationDTO response = service.save(reservation);
         return  new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     public ResponseEntity<ReservationDTO> fallbackPost(ReservationDTO reservation, RequestNotPermitted rnpe) {
-        System.out.println("calling to fallbackPost");
+        LOGGER.debug("calling to fallbackPost");
         throw new ReservationException(APIError.EXCEED_REQUEST_LIMIT);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ReservationDTO> update(@Min(1) @PathVariable Long id, @Valid @RequestBody ReservationDTO reservation) {
+        LOGGER.info("Updating a reservation with {}", id);
         ReservationDTO response = service.update(id, reservation);
         return  new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@Min(1) @PathVariable Long id) {
+        LOGGER.info("Deleting a reservation with id {}", id);
         service.delete(id);
         return  new ResponseEntity<>(HttpStatus.OK);
     }
